58658cefdfa13edd20aee06a458af1b4af594766
张梦林 <zhangml5511@163.com>
2018-09-01 14:49:29 +0800

    先将两个数据进行合并成一个新的数组，然后在新的数组中查找中位数

--- src/com/zhangxin/demo_2018_09_01/Demo1.java
+++ src/com/zhangxin/demo_2018_09_01/Demo1.java
@@ -8,7 +8,6 @@
 
 package com.zhangxin.demo_2018_09_01;
 
-import java.util.Arrays;
 
 public class Demo1 {
 
@@ -19,42 +18,26 @@ public class Demo1 {
         System.out.println(demo.findMedianSortedArrays(nums1, nums2));
     }
 
-    //    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-//        int l1 = nums1.length;
-//        int l2 = nums2.length;
-//        int i = 0;
-//        int j = 0;
-//        int cur = 0;
-//        int[] val = new int[l1+l2];
-//        while(i < l1 && j < l2){
-//            val[cur++] = (nums1[i] < nums2[j])? nums1[i++] : nums2[j++];
-//        }
-//        while(j < l2){
-//            val[cur++] = nums2[j++];
-//        }
-//        while(i < l1){
-//            val[cur++] = nums1[i++];
-//        }
-//        if ((l1 + l2) % 2 == 0){
-//            return (val[(l1 + l2) / 2 - 1] + val[(l1 + l2) / 2])/2.0;
-//        }else{
-//            return val[(l1 + l2) / 2];
-//        }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length, left = (m + n + 1) / 2, right = (m + n + 2) / 2;
-        return (findKth(nums1, nums2, left) + findKth(nums1, nums2, right)) / 2.0;
-    }
-
-    int findKth(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length, n = nums2.length;
-        if (m > n) return findKth(nums2, nums1, k);
-        if (m == 0) return nums2[k - 1];
-        if (k == 1) return Math.min(nums1[0], nums2[0]);
-        int i = Math.min(m, k / 2), j = Math.min(n, k / 2);
-        if (nums1[i - 1] > nums2[j - 1]) {
-            return findKth(nums1, Arrays.copyOfRange(nums2, j, n), k - j);
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int i = 0;
+        int j = 0;
+        int cur = 0;
+        int[] val = new int[l1 + l2];
+        while (i < l1 && j < l2) {
+            val[cur++] = (nums1[i] < nums2[j]) ? nums1[i++] : nums2[j++];
+        }
+        while (j < l2) {
+            val[cur++] = nums2[j++];
+        }
+        while (i < l1) {
+            val[cur++] = nums1[i++];
+        }
+        if ((l1 + l2) % 2 == 0) {
+            return (val[(l1 + l2) / 2 - 1] + val[(l1 + l2) / 2]) / 2.0;
         } else {
-            return findKth(Arrays.copyOfRange(nums1, i, m), nums2, k - i);
+            return val[(l1 + l2) / 2];
         }
     }
 }
